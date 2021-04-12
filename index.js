ee465ed99d68df9fecba0c43ab666df992a956e8
MarkBracke <mark.bracke@tactics.be>
2021-04-12 08:34:20 +0200

    Added jwt token as requested by the miro dev docs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
 exports.handler = async (event) => {
+    var jwt = require('jsonwebtoken');
+    let token = jwt.sign({}, process.env.MIRO_CLIENT_SECRET, { issuer: process.env.MIRO_CLIENT_ID, expiresIn: Date.UTC() + 300000 }); //Expires in 5 minutes (5 * 60 * 1000)
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify("Hello world"),
+        body: token
     };
     return response;
 };
