05dfb5ff5211f8863abe3f11df986584aa6d4a94
MarkBracke <mark.bracke@tactics.be>
2021-04-12 08:40:15 +0200

    Changed expiresIn into a string representing a timespan

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 exports.handler = async (event) => {
     var jwt = require('jsonwebtoken');
-    let token = jwt.sign({}, process.env.MIRO_CLIENT_SECRET, { issuer: process.env.MIRO_CLIENT_ID, expiresIn: Date.UTC() + 300000 }); //Expires in 5 minutes (5 * 60 * 1000)
+    let token = jwt.sign({}, process.env.MIRO_CLIENT_SECRET, { issuer: process.env.MIRO_CLIENT_ID, expiresIn: "00:05:00" });
 
     const response = {
         statusCode: 200,
